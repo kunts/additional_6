c861cacdcf8706e2d2738f9f0bcf2c324f8072bf
Alena.Kuntsevich <ekuntsevich@yandex.ru>
2017-09-21 20:26:57 +0300

    Factorial for small numbers and zero count

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function zeros(expression) {
-  // your solution
+        var tempRes = fact(parseInt(expression));
+        var zeroAmount = 0;
+        var i = true;
+        var t = 10;
+        while (i){
+
+        if((tempRes % (t)) === 0){
+          zeroAmount++;
+
+        } else {
+
+          i = false;
+
+        }
+
+        t = t*10;
+      }
+return zeroAmount;
+}
+
+
+
+function fact(a){
+var res;
+if(a<=1){
+res = 1;
+} else {
+res = a*fact(a-1);
+}
+return res;
 }
